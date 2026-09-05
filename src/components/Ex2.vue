44347edfbe102a91b8539ff5df6a104e9947a88a
faithpho <faith.ho.2025@smu.edu.sg>
2026-09-05 13:10:20 +0800

    Complete Made changes to Ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,6 +3,7 @@
 
     // Add code here
     import me from "/assets/me.png"
+    import wad from "/assets/wad.png"
     const width = ref(300);
     const height = ref(200);
 
@@ -19,9 +20,9 @@
 
     <!-- Add code here -->
     <!-- single attribute binding -->
-    <img v-bind:src="me" v-bind:width="width" v-bind:height="height">
+    <img v-bind:src="wad" v-bind:width="width" v-bind:height="height" alt="wad logo">
 
     <!-- multiple attribute binding -->
-    <img v-bind="imgAttrs">
+    <img v-bind="imgAttrs" alt="me">
 
 </template>
